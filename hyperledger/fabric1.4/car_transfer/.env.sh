056f98470407c075e0dddb91bab428e8ced0cb62
ys-sa <yssa@kakao.com>
2021-08-29 22:39:37 +0000

    add composer

--- hyperledger/fabric1.4/car_transfer/.env.sh
+++ hyperledger/fabric1.4/car_transfer/.env.sh
@@ -6,3 +6,23 @@ git clone -b v1.4.9 --depth=1 -c advice.detachedHead=false https://github.com/hy
 popd
 go get -u github.com/jinzhu/inflection
 go get -u github.com/stretchr/testify/assert
+
+# install go vendor
+go get -u github.com/kardianos/govendor
+
+# make directory to build
+mkdir .build
+cp -r ./chaincode/* ./.build
+export GOPATH=`pwd`/.build:`pwd`/fabric
+export PATH=`pwd`/.build/bin:$PATH
+
+# vendoring
+pushd .build/src/cartransfer/main
+govendor init
+govendor add cartransfer
+govendor add cartransfer/chaincode
+govendor add github.com/jinzhu/inflection
+popd
+
+# dist
+mkdir -p dist && cd ./build && cp -r --parents src/cartransfer/main ../dist && cd ..
